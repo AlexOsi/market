9ecc9d0d1222ba7441aeba61a002de458b271e9b
Alexander Osintsev <alexosintsev.job@gmail.com>
2022-01-07 05:50:51 +0200

    Fixed @RequestBody dependency due to incorrect validation.

--- src/main/java/com/osintsev/market/rest/controllers/BeatController.java
+++ src/main/java/com/osintsev/market/rest/controllers/BeatController.java
@@ -4,16 +4,12 @@ import com.osintsev.market.goods.BeatsServiceImpl;
 import com.osintsev.market.rest.dto.Beat;
 import com.osintsev.market.rest.dto.Beats;
 import com.osintsev.market.exception.BeatNotFoundException;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
