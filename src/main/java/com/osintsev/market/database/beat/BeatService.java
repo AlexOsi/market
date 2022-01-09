d5bf5a496d03eb411318e0ba4e976dca6bb28342
Alexander Osintsev <alexosintsev.job@gmail.com>
2022-01-09 09:14:34 +0200

    BeatService. Look previous commit

---
+++ src/main/java/com/osintsev/market/database/beat/BeatService.java
@@ -0,0 +1,10 @@
+package com.osintsev.market.database.beat;
+
+import com.osintsev.market.exception.BeatNotFoundException;
+import com.osintsev.market.rest.dto.Beat;
+import com.osintsev.market.rest.dto.Beats;
+
+public interface BeatService {
+    Beats getBeats();
+    Beat getBeat(Long id) throws BeatNotFoundException;
+}
--- src/main/java/com/osintsev/market/goods/BeatService.java
+++
@@ -1,9 +0,0 @@
-package com.osintsev.market.goods;
-
-import com.osintsev.market.rest.dto.Beat;
-import com.osintsev.market.rest.dto.Beats;
-
-public interface BeatService {
-    Beats getBeats();
-    Beat getBeat(Long id);
-}
